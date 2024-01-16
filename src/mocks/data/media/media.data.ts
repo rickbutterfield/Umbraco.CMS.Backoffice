15ca680b8df0d09e6500c29d6eff3c6954e69895
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2024-01-16 19:33:05 +1300

    test mock data

--- src/mocks/data/media/media.data.ts
+++ src/mocks/data/media/media.data.ts
@@ -2,6 +2,7 @@ import {
 	MediaItemResponseModel,
 	DocumentResponseModel,
 	MediaTreeItemResponseModel,
+	ContentStateModel,
 } from '@umbraco-cms/backoffice/backend-api';
 
 type TODO_FIX_MODEL_MediaResponseModel = DocumentResponseModel;
@@ -12,4 +13,41 @@ export type UmbMockMediaModelHack = TODO_FIX_MODEL_MediaResponseModel &
 
 export interface UmbMockMediaModel extends Omit<UmbMockMediaModelHack, 'type'> {}
 
-export const data: Array<UmbMockMediaModel> = [];
+export const data: Array<UmbMockMediaModel> = [
+	{
+		name: 'Flipped Car',
+		hasChildren: false,
+		isContainer: false,
+		icon: 'icon-document',
+		noAccess: false,
+		values: [
+			{
+				culture: 'string',
+				segment: 'string',
+				alias: 'string',
+				value: 'string',
+			},
+		],
+		variants: [
+			{
+				culture: 'string',
+				segment: 'string',
+				name: 'string',
+				createDate: '2024-01-16T06:28:45.451Z',
+				updateDate: '2024-01-16T06:28:45.451Z',
+				state: ContentStateModel.PUBLISHED,
+				publishDate: '2024-01-16T06:28:45.451Z',
+			},
+		],
+		id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+		contentTypeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+		urls: [
+			{
+				culture: 'string',
+				url: 'string',
+			},
+		],
+		templateId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+		isTrashed: true,
+	},
+];
