a78286918e8391da5901a276e7f92a77f2632247
Mads Rasmussen <madsr@hey.com>
2024-08-12 15:41:00 +0200

    check for read permissions in document tree item

--- src/packages/documents/documents/tree/tree-item/document-tree-item.context.ts
+++ src/packages/documents/documents/tree/tree-item/document-tree-item.context.ts
@@ -1,4 +1,5 @@
 import type { UmbDocumentTreeItemModel, UmbDocumentTreeRootModel } from '../types.js';
+import { UMB_USER_PERMISSION_DOCUMENT_READ, UmbDocumentUserPermissionCondition } from '../../user-permissions/index.js';
 import { UmbDefaultTreeItemContext } from '@umbraco-cms/backoffice/tree';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbIsTrashedEntityContext } from '@umbraco-cms/backoffice/recycle-bin';
@@ -9,14 +10,48 @@ export class UmbDocumentTreeItemContext extends UmbDefaultTreeItemContext<
 > {
 	// TODO: Provide this together with the EntityContext, ideally this takes part via a extension-type [NL]
 	#isTrashedContext = new UmbIsTrashedEntityContext(this);
-
 	readonly isTrashed = this._treeItem.asObservablePart((item) => item?.isTrashed ?? false);
+
+	#hasReadPermission = false;
 
 	constructor(host: UmbControllerHost) {
 		super(host);
 
-		this.observe(this.isTrashed, (isTrashed) => {
-			this.#isTrashedContext.setIsTrashed(isTrashed);
+		this._isDisabled.setValue(true);
+
+		this.#observeIsTrashed();
+		this.#observeReadDocumentUserPermission();
+	}
+
+	override checkIsDisabled(): void {
+		super.checkIsDisabled();
+
+		if (!this.#hasReadPermission) {
+			this._isDisabled.setValue(true);
+		}
+	}
+
+	#observeIsTrashed() {
+		this.observe(
+			this.isTrashed,
+			(isTrashed) => {
+				this.#isTrashedContext.setIsTrashed(isTrashed);
+			},
+			'umbObserveIsTrashed',
+		);
+	}
+
+	#observeReadDocumentUserPermission() {
+		const condition = new UmbDocumentUserPermissionCondition(this, {
+			host: this,
+			config: {
+				alias: 'Umb.Condition.UserPermission.Document',
+				oneOf: [UMB_USER_PERMISSION_DOCUMENT_READ],
+			},
+			onChange: () => {
+				this.#hasReadPermission = condition.permitted;
+				this.checkIsDisabled();
+			},
 		});
 	}
 }
